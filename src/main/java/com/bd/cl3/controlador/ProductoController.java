e204da71b217fcffee1ffefd6d2dd8c3e88e1247
Jorge Torres <jorgetorresmora99@gmail.com>
2023-11-25 23:45:45 -0500

    Actualizar funcional

--- src/main/java/com/bd/cl3/controlador/ProductoController.java
+++ src/main/java/com/bd/cl3/controlador/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -63,4 +64,12 @@ public class ProductoController {
 		return "Vistas/Producto/FrmProducto";
 	}
 	
+	//ELIMINAR
+	@GetMapping("/Eliminar/{id}")
+	public String EliminarProducto(@PathVariable("id") Integer id) {
+		//borramos el objeto
+		servicio.BorrarProducto(id);
+		//retornar al listado
+		return "redirect:/Producto/Listado";
+	}
 }
