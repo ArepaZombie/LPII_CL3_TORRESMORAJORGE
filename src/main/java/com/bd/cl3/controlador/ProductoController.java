6b97e6ab0a36c26f3c92ed2a04c7b3eec898dcbe
Jorge Torres <jorgetorresmora99@gmail.com>
2023-11-25 23:41:09 -0500

    Registrar funcional

--- src/main/java/com/bd/cl3/controlador/ProductoController.java
+++ src/main/java/com/bd/cl3/controlador/ProductoController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.bd.autos.modelo.ClassAuto;
 import com.bd.cl3.modelo.Producto;
 import com.bd.cl3.servicios.ProductoServicioImp;
 
@@ -39,6 +41,26 @@ public class ProductoController {
 		model.addAttribute("producto",p);
 		//retornar a la vista
 		return "Vistas/Producto/FrmProducto";
-	}//fin registrarauto
+	}
+	
+	//GUARDAR
+	@PostMapping("/Guardar")
+	public String GuardarProducto(@ModelAttribute Producto producto) {	
+		//aplicamos inyeccion
+		servicio.RegistrarProducto(producto);
+		//retornar al listado
+		return "redirect:/Producto/Listado";
+	}
+	
+	//REGISTRAR
+	@GetMapping("/Editar/{id}")
+	public String EditarProducto(@PathVariable("id") Integer id, Model model) {
+		//buscamos el objeto
+		Producto p = servicio.BuscarProducto(id);
+		//enviamos objeto a la vista
+		model.addAttribute("producto",p);
+		//retornar a la vista
+		return "Vistas/Producto/FrmProducto";
+	}
 	
 }
