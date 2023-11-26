1862599387d0d1b9cae39769d2f9daea5b4cffa8
Jorge Torres <jorgetorresmora99@gmail.com>
2023-11-25 22:44:34 -0500

    Creación de Proyecto

---
+++ src/main/java/com/bd/cl3/LpiiCl3TorresmorajorgeApplication.java
@@ -0,0 +1,13 @@
+package com.bd.cl3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiCl3TorresmorajorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiCl3TorresmorajorgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/cl3/LpiiCl3TorresmorajorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.cl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiCl3TorresmorajorgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
